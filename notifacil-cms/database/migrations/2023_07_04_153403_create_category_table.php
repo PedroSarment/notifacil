a6bcb9d6ede4f579e4e795fd102cd15b6b88f498
PedroSarment <phsp@cin.ufpe.br>
2023-07-04 12:40:08 -0300

    fix foreign key error

--- notifacil-cms/database/migrations/2023_07_04_153403_create_category_table.php
+++ notifacil-cms/database/migrations/2023_07_04_153403_create_category_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('title');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
             $table->foreign('created_by')->references('id')->on('users');
             $table->foreign('updated_by')->references('id')->on('users');
         });
