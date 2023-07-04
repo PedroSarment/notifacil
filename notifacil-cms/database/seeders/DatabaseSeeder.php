8d751f49704ffae2dd83de36668514812d8bc3b1
PedroSarment <phsp@cin.ufpe.br>
2023-07-04 14:13:08 -0300

    add database factories and seeders

--- notifacil-cms/database/seeders/DatabaseSeeder.php
+++ notifacil-cms/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\News;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(20)->create();
+        News::factory(20)->create();
+        Category::factory(20)->create();
     }
 }
