3925f416bfa1b0b5fc173005d38f9d5e6bc32cb2
PedroSarment <phsp@cin.ufpe.br>
2023-07-04 12:33:21 -0300

    Adds news migration

---
+++ notifacil-cms/database/migrations/2023_07_04_152902_create_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->longText('content')->nullable();
+            $table->integer('views');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->boolean('public');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
