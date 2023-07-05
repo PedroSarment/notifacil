220847826cbc80065dd68729db1fc378bce4278b
PedroSarment <phsp@cin.ufpe.br>
2023-07-05 00:03:28 -0300

    create controller and routes for news

--- notifacil-cms/app/Http/Controllers/NewsController.php
+++ notifacil-cms/app/Http/Controllers/NewsController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\News;
+use App\Models\Category;
+use Carbon\Carbon;
+use App\Http\Requests\NewsRequest;
+
 
 class NewsController extends Controller
 {
-    private static function render($state = 'index', $news = null)
+    private static function render($state = 'index', $news = null, $categorys = null)
     {
-       return Inertia::render('News/Index', [ 'news' => $news, 'state' => $state ]);
+       return Inertia::render('News/Index', [ 'news' => $news, 'state' => $state, 'categorys' => $categorys ]);
     }
 
     public function index()
@@ -22,16 +26,125 @@ class NewsController extends Controller
         return self::render('index', $news);
     }
 
-    public function view($id)
+    public function create()
+    {
+        $categorys = Category::all();
+
+        throw_if(!$categorys, GeneralException::class, 'Falha ao tentar acessar o Banco de Dados');
+
+        $categorysArray = [];
+        
+        foreach($categorys as $category) {
+            $categorysArray[] = [
+                'title' => $category->title,
+                'id' => $category->id
+            ];
+        }
+
+        return self::render('creating', null, $categorys); 
+    }
+
+    public function store(NewsRequest $request)
+    {
+        $requestValidated = $request->validated();
+        $requestValidated['views'] = 0;
+        $news = new News($requestValidated);
+        $userCreating = $request->user();
+        $userUpdated = $userCreating->News()->save($news);
+
+        throw_if(!$news || !$userUpdated, GeneralException::class, 'Não foi possível salvar a Notícia');
+
+        return redirect()->route('Noticias')->with('message', 'Notícia Cadastrada com Sucesso!');
+    }
+
+    public function edit(Int $id)
+    {
+        $categorys = Category::all();
+        $news = News::find($id);
+
+        throw_if(!$categorys || !$news , GeneralException::class, 'Falha ao tentar acessar o Banco de Dados');
+
+        $categorysArray = [];
+        
+        foreach($categorys as $category) {
+            $categorysArray[] = [
+                'title' => $category->title,
+                'id' => $category->id
+            ];
+        }
+
+        return self::render('editing', $news, $categorys); 
+    }
+
+    public function update(NewsRequest $request, Int $id)
+    {
+        $requestValidated = $request->validated();
+        $requestValidated['updated_by'] = $request->user()->id;
+        $news = News::find($id);
+        $news->update($request->validated());
+
+        throw_if(!$news, GeneralException::class, 'Não foi possível atualizar a Notícia');
+
+        return redirect()->route('Noticias')->with('message', 'Notícia Atualizada com Sucesso!');
+    }
+
+    public function view(Int $id)
     {
         $news = News::with('Created_by')->with('Category')->get();
         $news = $news->find($id);
         $news->update([ 'views' => $news->views + 1 ]);
-        $news->created_at = '12/12/12';
+
+        throw_if(!$news, GeneralException::class, 'Falha ao tentar acessar o Banco de Dados');
+       
+        $news = [
+            'id' => $news->id,
+            'views' => $news->views,
+            'created_at' => self::formatDateTime($news->created_at),
+            'title' => $news->title,
+            'created_by' => $news->Created_by,
+            'content' => $news->content,
+            'summary' => $news->summary,
+            'category' => $news->Category,
+        ];
+      
+        return self::render('viewing', $news);
+    }
+
+    public function delete(Int $id)
+    {
+        $news = News::find($id);
 
         throw_if(!$news, GeneralException::class, 'Falha ao tentar acessar o Banco de Dados');
 
-        return self::render('viewing', $news);
+        $news = [
+            'id' => $news->id,
+            'views' => $news->views,
+            'created_at' => self::formatDateTime($news->created_at),
+            'title' => $news->title,
+            'created_by' => $news->Created_by,
+            'content' => $news->content,
+            'summary' => $news->summary,
+            'category' => $news->Category,
+        ];
+
+        return self::render('deleting', $news, null);
+    }
+
+    public function destroy(Int $id)
+    {
+        $news = News::find($id);
+
+        $deleted = $news->delete();
+
+        throw_if(!$deleted, GeneralException::class, 'Não foi possível excluir a notícia');
+
+        return redirect()->route('Noticias')->with('message', 'Noticia excluída com sucesso!');
+    }
+
+    private static function formatDateTime($data)
+    {
+        $data = Carbon::parse($data);
+        return $data->format('d/m/Y h:i:s');
     }
 
 }
--- notifacil-cms/routes/web.php
+++ notifacil-cms/routes/web.php
@@ -16,20 +16,31 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+    return redirect()->route('login');
+})->name('home');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', ])->group(function () {
+
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+    Route::get('/noticias', function () {
+        return Inertia::render('Dashboard');
+    })->name('noticias');
+    Route::get('/categorias', function () {
+        return Inertia::render('Welcome');
+    })->name('categorias');
+
+    Route::controller(App\Http\Controllers\NewsController::Class)->group(function () {
+
+        Route::get('/noticias', 'index')->name('Noticias');
+        Route::get('/noticias/cadastro', 'create')->name('Noticias/Cadastro');
+        Route::post('/noticias/salva', 'store')->name('Noticias/Salva');
+        Route::get('/noticias/edita/{id}', 'edit')->name('Noticias/Edita');
+        Route::get('/noticias/exclui/{id}', 'delete')->name('Noticias/Exclui');
+        Route::delete('/noticias/apaga/{id}', 'destroy')->name('Noticias/Apaga');
+        Route::post('/noticias/atualiza/{id}', 'update')->name('Noticias/Atualiza');
+        Route::get('/noticias/{id}', 'view')->name('Noticias/View');
+    });
+
 });
