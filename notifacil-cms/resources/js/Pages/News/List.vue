dfbabf4aa6b58fadef660318c727a6413b4157d2
PedroSarment <phsp@cin.ufpe.br>
2023-07-05 00:02:32 -0300

    add paginated list in frontend

--- notifacil-cms/resources/js/Pages/News/List.vue
+++ notifacil-cms/resources/js/Pages/News/List.vue
@@ -4,6 +4,8 @@ import PageTitle from '@/Components/PageTitle.vue';
 import WhiteCard from '@/Components/WhiteCard.vue';
 import NewsItem from './NewsItem.vue'
 import Pagination from '@/Components/Pagination.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { Link } from '@inertiajs/vue3';
 
 
 const props = defineProps({
@@ -31,9 +33,12 @@ function convertToDateTimeFormat(dateString) {
 <template>
 
     <div class="flex flex-col gap-4">
-                    
-        <PageTitle class="mb-2">Notícias</PageTitle>
 
+        <div class="flex flex-row w-full justify-between">
+            <PageTitle class="mb-2">Notícias</PageTitle>
+            <Link href="/noticias/cadastro"><PrimaryButton>+</PrimaryButton></Link>
+        </div>
+                    
         <div  v-for="newsItem in news.data">
             
             <NewsItem :views="newsItem.views" :author="newsItem.created_by.name" :category="newsItem.category.title" :title="newsItem.title" :date="convertToDateTimeFormat(newsItem.created_at)" :id="newsItem.id" :summary="newsItem.summary"/>
