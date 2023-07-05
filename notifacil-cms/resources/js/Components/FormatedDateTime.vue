79514d84d04119a345b13a0da208d98fdd2564ad
PedroSarment <phsp@cin.ufpe.br>
2023-07-05 08:38:37 -0300

    create component to format dates

---
+++ notifacil-cms/resources/js/Components/FormatedDateTime.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+const props = defineProps({
+    date: {
+        type: String,
+        default: ''
+    }
+});
+
+function convertToDateTimeFormat(dateString) {
+  
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+
+    return `${day}/${month}/${year} ${hour}:${minute}:${second}`;
+}
+
+</script>
+
+<template>
+    {{convertToDateTimeFormat(date)}}
+</template>
