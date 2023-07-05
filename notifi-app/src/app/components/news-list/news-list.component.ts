3d4e5688eff48878c40e50a5ae712bea69a88370
PedroSarment <phsp@cin.ufpe.br>
2023-07-05 19:04:43 -0300

    create news list

---
+++ notifi-app/src/app/components/news-list/news-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-news-list',
+  templateUrl: './news-list.component.html',
+  styleUrls: ['./news-list.component.scss'],
+})
+export class NewsListComponent{
+
+  constructor() { }
+
+  @Input() news: any[] = []
+
+  formatCreatedAt(created_at: string): string {
+    const date = new Date(created_at);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+  }
+
+}
