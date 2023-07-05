725afc5a10ffd62850db1716be7899afe85c65cf
PedroSarment <phsp@cin.ufpe.br>
2023-07-05 00:01:46 -0300

    add icons to the frontend

--- notifacil-cms/resources/js/Components/Status.vue
+++ notifacil-cms/resources/js/Components/Status.vue
@@ -10,13 +10,13 @@ defineProps({
 
 <template>
 
-    <div class="flex flex-row gap-4 bg-white rounded shadow-md border border-border-input p-4 items-center">
+    <div class="flex flex-row gap-4 bg-white rounded shadow-md border border-slate p-4 items-center">
 
         <img v-if="type == 'error'" class="w-[25px]" src="/assets/icons/WarningOutlined.svg"/>
 
         <img v-if="type == 'sucess'" class="w-[25px]" src="/assets/icons/CheckCircleOutlined.svg"/>
 
-        <p class="text-xl"> {{status}} </p>
+        <p class="text-sm"> {{status}} </p>
 
     </div>
 </template>
